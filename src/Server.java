54a7804525c46606b7fb9d3cf337b47c7bd1a8dd
Martin Pettersson <kontakt@martinpettersson.se>
2016-02-24 20:38:47 +0100

    Basic server added

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,36 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by martinpettersson on 24/02/16.
  */
 public class Server {
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    public Server() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1337);
+            Socket socket = serverSocket.accept();
+            DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String receivedMessage = "";
+            String transferredMessage = "";
+
+            while (!receivedMessage.equals("end")) {
+                receivedMessage = dataIn.readUTF();
+                System.out.println(receivedMessage);
+                transferredMessage = reader.readLine();
+                dataOut.writeUTF(transferredMessage);
+                dataOut.flush();
+            }
+            
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
