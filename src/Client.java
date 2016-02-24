cf4167ab856ede2d5a632f9e5c32c5ffd24ece90
Martin Pettersson <kontakt@martinpettersson.se>
2016-02-25 00:47:49 +0100

    Client threads

--- src/Client.java
+++ src/Client.java
@@ -4,11 +4,13 @@ import java.net.Socket;
 /**
  * Created by martinpettersson on 24/02/16.
  */
-public class Client {
+public class Client implements Runnable{
     private Socket socket;
     private DataInputStream dataIn;
     private DataOutputStream dataOut;
     private BufferedReader reader;
+    private ClientThread clientThread    = null;
+    private Thread thread              = null;
 
     public static void main(String args[]) {
         if (args.length != 2)
@@ -18,31 +20,91 @@ public class Client {
     }
 
     public Client(String serverName, int portNumber) {
-        try {
+
+        System.out.println("Establishing connection. Please wait ...");
+            try {
             socket = new Socket(serverName, portNumber);
             System.out.println("Connected");
             startClient();
-            String receivedMessage = ""; String transferredMessage = "";
-
-            while (!receivedMessage.equals("end")) {
-                transferredMessage = reader.readLine();
-                dataOut.writeUTF(transferredMessage);
-                receivedMessage = dataIn.readUTF();
-                System.out.println(receivedMessage);
-            }
-
-            dataIn.close();
-            dataOut.close();
-            socket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void run()
+    {  while (thread != null)
+        {  try
+            {
+                String transferredMessage = reader.readLine();
+                dataOut.writeUTF(transferredMessage);
+                dataOut.flush();
+            }
+            catch(IOException ioe)
+            {
+        }
+    }
+
+    }
+
+    public void handle(String msg)
+    {  if (msg.equals(".bye"))
+    {  System.out.println("Good bye. Press RETURN to exit ...");
+    }
+    else
+        System.out.println(msg);
+    }
+
+
+
     public void startClient() throws IOException {
         dataIn = new DataInputStream(socket.getInputStream());
         dataOut = new DataOutputStream(socket.getOutputStream());
         reader = new BufferedReader(new InputStreamReader(System.in));
+
+        if (thread == null)
+        {  clientThread = new ClientThread(this, socket);
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    class ClientThread extends Thread
+    {  private Socket           socket   = null;
+        private Client       client   = null;
+        private DataInputStream  streamIn = null;
+
+        public ClientThread(Client _client, Socket _socket)
+        {  client   = _client;
+            socket   = _socket;
+            open();
+            start();
+        }
+        public void open()
+        {  try
+        {  streamIn  = new DataInputStream(socket.getInputStream());
+        }
+        catch(IOException ioe)
+        {  System.out.println("Error getting input stream: " + ioe);
+        }
+        }
+        public void close()
+        {  try
+        {  if (streamIn != null) streamIn.close();
+        }
+        catch(IOException ioe)
+        {  System.out.println("Error closing input stream: " + ioe);
+        }
+        }
+        public void run()
+        {  while (true)
+        {  try
+        {  client.handle(streamIn.readUTF());
+        }
+        catch(IOException ioe)
+        {  System.out.println("Listening error: " + ioe.getMessage());
+        }
+        }
+        }
     }
 }
